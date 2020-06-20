9b4cae9fc1719f9d7f2e3bf5bd2b77f621099318
Bi0x <Bi0x@foxmail.com>
2020-06-20 22:50:48 +0800

    Finished

--- crawler.py
+++ crawler.py
@@ -1,7 +1,18 @@
 import requests
-import re, time
+import re
+import time, random
 #import pprint
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
+
+fakeHeaders = [
+    {
+        'User-Agent': 'User-Agent:Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36'
+    }, {
+        'User-Agent': 'User-Agent:Mozilla/5.0(Macintosh;U;IntelMacOSX10_6_8;en-us)AppleWebKit/534.50(KHTML,likeGecko)Version/5.1Safari/534.50'
+    }, {
+        'User-Agent': 'User-Agent:Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1;360SE)'
+    }
+]
 
 
 def regexSearcher(htmlPage, regexParser):
@@ -19,62 +30,53 @@ def weatherTranslate(str):
     raise Exception
 
 # http://www.tianqihoubao.com/
-
-
-def getFromTianqihoubao(nowYear=2020, nowMonth=1, duration=5, city="jinhua"):
+def getFromTianqihoubao(nowYear=2020, nowMonth=1, city="jinhua"):
     result = {}
-    result['tempareture'] = []
-    result['sunny'] = []
-    result['cloudy'] = []
-    result['rainy'] = []
-    for k in range(duration):
-        if nowMonth > 12:
-            nowYear += 1
-            nowMonth = 1
-        url = "http://www.tianqihoubao.com/lishi/%s/month/" % city
-        url += str(nowYear) + str(nowMonth).rjust(2, '0') + ".html"
-        print(url)
-        htmlPage = requests.get(url).text
-        temparetureParser = "<td>\s*(\d*)℃\s*/\s*(\d*)℃\s*</td>"
-        result['tempareture'].append(
-            regexSearcher(htmlPage, temparetureParser)
-        )
-        weatherParser = "<td>\s*([\u4e00-\u9fa5]*)\s*/([\u4e00-\u9fa5]*)"
-        resTmp = regexSearcher(htmlPage, weatherParser)
-        result['sunny'].append(0)
-        result['cloudy'].append(0)
-        result['rainy'].append(0)
-        # 由于每天的天气都是XX转XX，故统计的合计天数会翻倍
-        for i in resTmp:
-            result[weatherTranslate(i[0])][k] += 1
-            result[weatherTranslate(i[1])][k] += 1
-        # pprint.pprint(result)
-        nowMonth += 1
+    result['days'] = []
+    result['sunny'] = 0
+    result['cloudy'] = 0
+    result['rainy'] = 0
+    url = "http://www.tianqihoubao.com/lishi/%s/month/" % city
+    url += str(nowYear) + str(nowMonth).rjust(2, '0') + ".html"
+    #print(url)
+    htmlPage = requests.get(url).text
+    temparetureParser = "<td>\s*(\d*)℃\s*/\s*(\d*)℃\s*</td>"
+    result['tempareture'] = regexSearcher(htmlPage, temparetureParser)
+    weatherParser = ">\s*\d*年\d*月(\d*)日\s*</a>\s*</td>\s*<td>\s*([\u4e00-\u9fa5]*)\s*/([\u4e00-\u9fa5]*)"
+    resTmp = regexSearcher(htmlPage, weatherParser)
+    for i in resTmp:
+        result[weatherTranslate(i[1])] += 1
+        result[weatherTranslate(i[2])] += 1
+        result['days'].append(i[0])
+    # pprint.pprint(result)
+    # 由于每天的天气都是XX转XX，统计的合计天数会翻倍，故全部除 2
+    result['sunny'] = 2
+    result['cloudy'] /= 2
+    result['rainy'] /= 2
+    #print(result)
     return result
 
 # http://lishi.tianqi.com/
-
-
-def getFromTianqi(nowYear=2020, nowMonth=1, duration=1, city="jinhua"):
+def getFromTianqi(nowYear=2020, nowMonth=1, city="jinhua"):
     result = {}
+    result['days'] = []
     result['tempareture'] = []
-    result['sunny'] = []
-    result['cloudy'] = []
-    result['rainy'] = []
-    for k in range(duration):
-        if nowMonth > 12:
-            nowYear += 1
-            nowMonth = 1
-        url = "http://lishi.tianqi.com/%s/" % city
-        url += str(nowYear) + str(nowMonth).rjust(2, '0') + ".html"
-        print(url)
-        headers = {
-            'User-Agent': 'User-Agent:Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36'
-        }
-        time.sleep(1)
-        htmlPage = requests.get(url, headers=headers).text
-        #soup = BeautifulSoup(htmlPage, 'html.parser')
-        
+    result['sunny'] = 0
+    result['cloudy'] = 0
+    result['rainy'] = 0
+    url = "http://lishi.tianqi.com/%s/" % city
+    url += str(nowYear) + str(nowMonth).rjust(2, '0') + ".html"
+    #print(url)
+    #time.sleep(1)
+    htmlPage = requests.get(url, headers=fakeHeaders[random.randint(0, 2)]).text
+    #soup = BeautifulSoup(htmlPage, 'html.parser')
+    #print(soup.find_all(class_='thrui'))
+    temparetureParser = '<div class="th\d*">\d*-\d*-(\d*).[\u4e00-\u9fa5]*.</div>\s*<div class="th\d*">(\d*)℃</div>\s*<div class="th\d*">(\d*)℃</div>\s*<div class="th\d*">([\u4e00-\u9fa5]*)</div>'
+    for i in regexSearcher(htmlPage, temparetureParser):
+        tmp = (i[1], i[2])
+        result[weatherTranslate(i[3])] += 1
+        result['tempareture'].append(tmp)
+        result['days'].append(i[0])
+    #print(result)
+    return result
 
-
-getFromTianqi()
--- main.py
+++ main.py
@@ -2,40 +2,52 @@ import matplotlib.pyplot as plt
 import calendar
 from crawler import *
 
-
-def getDays(year, month):
-    days31 = [1, 3, 5, 7, 8, 10, 12]
-    days30 = [4, 6, 9, 11]
-    if month in days31:
-        return 31
-    if month in days30:
-        return 30
-    if year % 4 == 0 and year % 100 != 0:
-        return 29
-    if year % 100 == 0 and year % 400 == 0:
-        return 29
-    return 28
-
-
 nowYear = 2020
 nowMonth = 1
-durationLong = 1
+durationLong = 5
 city = 'jinhua'
-result = getFromTianqihoubao(duration=durationLong)
+rainy = []
+sunny = []
+cloudy = []
 for k in range(durationLong):
     if nowMonth > 12:
         nowYear += 1
         nowMonth = 1
-    tmpDays = getDays(nowYear, nowMonth)
-    x_lines = [x for x in range(1, tmpDays + 1)]
-    y_lines = [int(x[0]) for x in result['tempareture'][k]]
+    try:
+        result = getFromTianqi(nowYear, nowMonth)
+    except:
+        result = getFromTianqihoubao(nowYear, nowMonth)
+    x_line = result['days']
+    highTemp_line = [int(x[0]) for x in result['tempareture']]
+    lowTemp_line = [int(x[1]) for x in result['tempareture']]
+    rainy.append(result['rainy'])
+    sunny.append(result['sunny'])
+    cloudy.append(result['cloudy'])
     title = city.title() + " Tempareture in " + \
-        calendar.month_abbr[nowMonth] + ' ' + str(nowYear)
+        calendar.month_abbr[nowMonth] + ' ' + str(nowYear)    
+    '''
+    #温度折线图
     plt.figure(k + 1)
     plt.title(title)
     plt.xlabel("Day")
     plt.ylabel("Temp/℃")
-    plt.plot(x_lines, y_lines, color='blue', marker='o')
-    plt.draw()
+    plt.tick_params(labelsize=6)
+    plt.plot(x_line, highTemp_line, color='blue', marker='o', label='Highest Temp')
+    plt.plot(x_line, lowTemp_line, color='green', marker='^', label='Lowest Temp')
+    plt.legend()
+    '''
     nowMonth += 1
+
+xlabel = [calendar.month_abbr[(x % 12) + 1] for x in range(durationLong)]
+x1_line = range(1, durationLong + 1)
+x2_line = [x + 0.25 for x in x1_line]
+x3_line = [x + 0.25 for x in x2_line]
+plt.figure("Total Weather")
+plt.title("Weather Crawler Result")
+plt.bar(x1_line, rainy, width=0.2, label="Rainy", color="orange")
+plt.bar(x2_line, sunny, width=0.2, label="Sunny", color="red")
+plt.bar(x3_line, cloudy, width=0.2, label="Cloudy", color="brown")
+plt.xticks([x + 0.25 for x in x1_line], xlabel)
+plt.tick_params(labelsize=60/durationLong)
+plt.legend()
 plt.show()
